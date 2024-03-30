bc02ea43bc92c832fbde9e857675f95d0863a218
MushtaqUmar <138302254+MushtaqUmar@users.noreply.github.com>
2024-03-30 19:40:56 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -8,7 +8,7 @@ let newGameBtn = document.querySelector("#new-btn");
 
 // track who's turn is (X or O)- Assume X starts First(playerx)
 // playerX, playerO
-let turnX = true; // if true, means print 'X' on box else 'O'
+let turnX = true; // if true, means print 'X' inside box , else 'O'
 
 //use 2D array for:  8 winning pattens (3 row wise, 3 column wise and 2 digonally)
 const winPatterns = [
@@ -55,37 +55,34 @@ const disableBoxes = () => {  //Or:  function disableBoxes(){ ... }
   }
 };
 
-
-
-// function to check winner
+// helper -> function to check winner
 function checkWinner() {
   for (let pattern of winPatterns) {
-    // winPatterns: is array above
     // pattern : current winning pattern (array itslef) under consideration
-    let pos1Val = boxes[pattern[0]].innerHTML; // pattern is array itself AND pattern[0] : gives element at index 0 in array pattern
+    let pos1Val = boxes[pattern[0]].innerHTML; // pattern is array itself AND pattern[0] : gives value at index 0 in array 'pattern'.
     let pos2Val = boxes[pattern[1]].innerHTML; // AND: boxes: list of boxes AND pattern[i] return index AND boxes[pattern[i]] gives box at that index
     let pos3Val = boxes[pattern[2]].innerHTML; // innerHTML : returns X or O at that box
     // index 0,1,2 to current pattern array is checked for content (if same declare winner)
 
-    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
-      // box must not be empty to check winning condition
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {      // box must not be empty to check winning condition
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
-        // we got winner (dont't allow now to enter X or O anymore )
+        // we got winner (dont't allow now to print X or O anymore for any click )
         disableBoxes(); // call
         // declare winner
         showWinner(pos1Val); // call
       }
     }
+    
   }
 }
 
 
 //1st MAIN LOGIC 
 
-// we need to do: pressing each button , print X or O
-var count = 0; // for checking whenther is draw or not
-boxes.forEach((box) => {
-  // for barticular Box where you click/press
+// we need to do: on click to each button , print X or O
+var count = 0; 
+boxes.forEach((box) => {  // for checking whether game is draw or not (total cells = 9)
+  // for particular Box where you click
   box.addEventListener("click", () => {
     if (turnX) {
       // in Js it is same as: if ( turnX == true)
@@ -99,23 +96,21 @@ boxes.forEach((box) => {
       turnX = true;
       count = count + 1; //new box added
     }
-    // Once current box is filled with X or O , make it disable (not allow again)
+    // Once current box is filled with X or O , make it disable (not allow to override the same cell)
     box.disabled = true;
+    
     // when we click , check immediately whether condition
-    checkWinner(); // call
-
+    checkWinner(); // call to helper function above
+    
     // When All boxes are filled AND no winner is seen
     if (count == 9) {
       gameDraw(); // call
     }
   });
-
-
+  
 });
 
-
-
-// When new game is started or game reset
+// helper func  -> When new game is started OR reset game
 function enableBoxes() {
   for (let box of boxes) {
     box.disabled = false;
@@ -124,13 +119,12 @@ function enableBoxes() {
   }
 }
 
-
 // to reset Game/ Start New Game
 function resetGame() {
   // when new game starts/reset game make:
   turnX = true; // as initially
   enableBoxes(); // all boxes - call to method
-  msgContainer.classList.add("hide"); // add class 'hide' back to html element fetched as msgCOntainer above
+  msgContainer.classList.add("hide"); // add class 'hide' back to html element fetched as 'msgContainer' above
 }
 
 // Above function applied when reset game/ start new game
